a0082390cc9ed205dd118714c7394269b55a4e1f
Tomohiro Koumura <tomohiro.koumura.gp@west.ntt.co.jp>
2018-07-24 13:31:57 +0900

    ボタンでスクロール位置が変化

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ var app = new Vue({
   data: {
     message: 'Hello, Vue.js!',
     list: ['aaa', 'bbb', 'ccc'],
-    count: 0,
+    buttonNums: [1, 2, 5, 10, 100],
     scroll: 0
   },
   mounted: function() {
     this.scroll = 100;
   },
   methods: {
-    increment() {
-      this.count++;
+    increment(num) {
+      this.scroll += num;
       console.log(event.target);
     }
   }
